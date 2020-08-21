0744e96129a4baf21d34d1ce6ceb0425c22f8418
bostoncleek <bocl8874@colorado.edu>
2020-08-21 11:54:46 -0500

    utils for processing clouds

---
+++ moveit_task_constructor_gpd/include/moveit_task_constructor_gpd/cloud_utils.h
@@ -0,0 +1,67 @@
+/*********************************************************************
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020 PickNik Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Boston Cleek
+   Desc:   Utility functions for processing point clouds
+*/
+
+#pragma once
+
+// C++
+#include <vector>
+
+// PCL
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+
+namespace moveit_task_constructor_gpd
+{
+typedef pcl::PointCloud<pcl::PointXYZRGBA> PointCloudRGBA;
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudRGB;
+
+/**
+* @brief Segments objects from table plane
+* @param [out] cloud - Segemented point cloud XYZRGB
+*/
+void removeTable(PointCloudRGB::Ptr cloud);
+
+/**
+* @brief Removes points outside the specified cartesian limits
+* @param xyz_lower - 3dim vector x,y,z lower limits on cloud
+* @param xyz_upper - 3dim vector x,y,z upper limits on cloud
+* @param [out] cloud - cloud XYZRGB
+*/
+void passThroughFilter(const std::vector<double> &xyz_lower,
+                       const std::vector<double> &xyz_upper,
+                       PointCloudRGB::Ptr cloud);
+} // namespace moveit_task_constructor_gpd
---
+++ moveit_task_constructor_gpd/src/moveit_task_constructor_gpd/cloud_utils.cpp
@@ -0,0 +1,114 @@
+/*********************************************************************
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020 PickNik Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Boston Cleek
+   Desc:   Utility functions for processing point clouds
+*/
+
+// ROS
+#include <ros/ros.h>
+
+// PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+
+#include <moveit_task_constructor_gpd/cloud_utils.h>
+
+namespace moveit_task_constructor_gpd
+{
+
+void removeTable(PointCloudRGB::Ptr cloud)
+{
+  // SAC segmentor without normals
+  pcl::SACSegmentation<pcl::PointXYZRGB> segmentor;
+  segmentor.setOptimizeCoefficients(true);
+  segmentor.setModelType(pcl::SACMODEL_PLANE);
+  segmentor.setMethodType(pcl::SAC_RANSAC);
+
+  // Max iterations and model tolerance
+  segmentor.setMaxIterations(1000);
+  segmentor.setDistanceThreshold (0.01);
+
+  // Input cloud
+  segmentor.setInputCloud(cloud);
+
+  // Inliers representing points in the plane
+  pcl::PointIndices::Ptr inliers_plane(new pcl::PointIndices);
+
+  // Use a plane as the model for the segmentor
+  pcl::ModelCoefficients::Ptr coefficients_plane(new pcl::ModelCoefficients);
+  segmentor.segment(*inliers_plane.get(), *coefficients_plane.get());
+
+  if (inliers_plane->indices.size () == 0)
+  {
+    ROS_ERROR("Could not estimate a planar model for the given dataset");
+  }
+
+  // Extract the inliers from the cloud
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract_indices;
+  extract_indices.setInputCloud(cloud);
+  extract_indices.setIndices(inliers_plane);
+
+  // Remove plane inliers and extract the rest
+  extract_indices.setNegative(true);
+  extract_indices.filter(*cloud.get());
+
+  if (cloud->points.empty())
+  {
+    ROS_ERROR("Point cloud is empty, segementation failed");
+  }
+}
+
+
+void passThroughFilter(const std::vector<double> &xyz_lower,
+                       const std::vector<double> &xyz_upper,
+                       PointCloudRGB::Ptr cloud)
+{
+  pcl::PassThrough<pcl::PointXYZRGB> pass;
+  pass.setInputCloud(cloud);
+
+  pass.setFilterFieldName("x");
+  pass.setFilterLimits(xyz_lower.at(0), xyz_upper.at(0));
+  pass.filter(*cloud.get());
+
+  pass.setFilterFieldName("y");
+  pass.setFilterLimits(xyz_lower.at(1), xyz_upper.at(1));
+  pass.filter(*cloud.get());
+
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(xyz_lower.at(2), xyz_upper.at(2));
+  pass.filter(*cloud.get());
+}
+} // namespace moveit_task_constructor_gpd
