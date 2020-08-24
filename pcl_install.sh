590dc57530652207ec4acce9c94b365c3f5a6fba
bostoncleek <bocl8874@colorado.edu>
2020-08-24 14:59:01 -0500

    pcl 1.11  install script

---
+++ pcl_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# install PCL 1.11 
+git clone https://github.com/PointCloudLibrary/pcl.git
+cd pcl
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+sudo make install
