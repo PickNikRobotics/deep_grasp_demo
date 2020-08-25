a3c2f86df07e70c2e13fdde2b4baa13240a8017e
bostoncleek <bocl8874@colorado.edu>
2020-08-25 13:57:13 -0500

    removed unwanted command

--- opencv_install.sh
+++ opencv_install.sh
@@ -2,27 +2,27 @@
 # install OpenCV 3.4
 
 # dependencies
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install build-essential
+sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
 # image processing
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
 # video processing
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt install libxvidcore-dev libx264-dev
 
 # GUI support
-sudo apt-get install libgtk-3-dev
+sudo apt install libgtk-3-dev
 
 # Optimization
-sudo apt-get install libatlas-base-dev gfortran pylint
+sudo apt install libatlas-base-dev gfortran pylint
 
 # Download and build OpenCV
 wget https://github.com/opencv/opencv/archive/3.4.0.zip -O opencv-3.4.0.zip
 wget https://github.com/opencv/opencv_contrib/archive/3.4.0.zip -O opencv_contrib-3.4.0.zip
 
-sudo apt-get install unzip
+sudo apt install unzip
 
 unzip opencv-3.4.0.zip
 unzip opencv_contrib-3.4.0.zip
