370ea586632687103a9ac7d51b6562a947a54c2f
bostoncleek <bocl8874@colorado.edu>
2020-08-25 12:05:25 -0500

    python pkg versions

--- dexnet_install.sh
+++ dexnet_install.sh
@@ -7,10 +7,10 @@ VERSION=$1 # cpu or gpu
 case "${VERSION}"
 in
 cpu)
-	pip install tensorflow
+	pip install tensorflow==1.15.0
 	;;
 gpu)
-  pip install tensorflow-gpu
+  pip install tensorflow-gpu==1.13.1
 	;;
 *)
 	echo "Usage: $0 {cpu|gpu}"
@@ -18,53 +18,40 @@ gpu)
 esac
 
 # install apt deps
-sudo apt-get install cmake libvtk5-dev python-vtk python-sip python-qt4 libosmesa6-dev meshlab libhdf5-dev
+sudo apt-get install cmake libvtk6-dev python-vtk6 python-sip python-qt4 libosmesa6-dev meshlab libhdf5-dev
 
 # install pip deps
-pip install numpy scipy scikit-learn scikit-image opencv-python pyassimp h5py mayavi matplotlib catkin_pkg multiprocess dill cvxopt ipython pillow pyhull setproctitle trimesh
+# python3 -m pip install numpy scipy scikit-learn scikit-image opencv-python Shapely h5py matplotlib catkin_pkg multiprocess dill cvxopt ipython pillow pyhull setproctitle trimesh
+python3 -m pip -r dexnet_requirements.txt
 
 # install deps from source
 mkdir dexnet_deps
 cd dexnet_deps
 
-# install SDFGen
-git clone https://github.com/jeffmahler/SDFGen.git
-cd SDFGen
-sudo sh install.sh
-cd ..
-
-# install Boost.NumPy
-git clone https://github.com/jeffmahler/Boost.NumPy.git
-cd Boost.NumPy
-sudo sh install.sh
-cd ..
-
-
 # install autolab modules
 git clone https://github.com/BerkeleyAutomation/autolab_core.git
 git clone https://github.com/BerkeleyAutomation/perception.git
 git clone https://github.com/BerkeleyAutomation/gqcnn.git
-git clone https://github.com/BerkeleyAutomation/meshpy.git
 git clone https://github.com/BerkeleyAutomation/visualization.git
 
 
 # install all Berkeley AUTOLAB modules
 # autolab_core
 cd autolab_core
-python setup.py develop
+sudo python3 setup.py develop
 cd ..
 
 # perception
 cd perception
-python setup.py develop
+sudo python3 setup.py develop
 cd ..
 
 # gqcnn
 cd gqcnn
-python setup.py develop
+sudo python3 setup.py develop
 cd ..
 
 # visualization
 cd visualization
-python setup.py develop
+sudo python3 setup.py develop
 cd ..
