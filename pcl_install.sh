00c740cb4613cb1d5715faf040db63962d01dece
bostoncleek <bocl8874@colorado.edu>
2020-09-01 14:22:30 -0500

    install pcl 1.11.0

--- pcl_install.sh
+++ pcl_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# install PCL 1.11
-
-git clone https://github.com/PointCloudLibrary/pcl.git
-cd pcl
+# install PCL 1.11.0
+wget https://github.com/PointCloudLibrary/pcl/archive/pcl-1.11.0.zip -O pcl-pcl-1.11.0.zip
+sudo apt install unzip
+unzip pcl-pcl-1.11.0.zip
+cd pcl-pcl-1.11.0
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j
+make -j4
 sudo make install
