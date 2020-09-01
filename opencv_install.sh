0bdf67c65053c08ed509b78f9cadc5ff0e96d199
bostoncleek <bocl8874@colorado.edu>
2020-09-01 14:23:10 -0500

    change to -j4

--- opencv_install.sh
+++ opencv_install.sh
@@ -32,7 +32,7 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -D WITH_CUDA=OFF -DCMAKE_INSTALL_PREFIX=/usr/local -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.4.0/modules -DOPENCV_ENABLE_NONFREE=True ..
-make -j
+make -j4
 sudo make install
 
 sudo ldconfig
